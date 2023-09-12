32369fe13d547fefbc1adcbe6ad1cedce1f5edac
axotolt <113329786+axotolt@users.noreply.github.com>
2023-09-12 15:38:03 +0700

    Add files via upload

--- Parallax Effect/src/App.jsx
+++ Parallax Effect/src/App.jsx
@@ -1,27 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useRef, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const menu = document.getElementById("menu");
+  const paraRef = useRef(null);
+  useEffect(() => {
+    const menu = document.getElementById("menu");
 
-  Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover =() => {
-      menu.dataset.activeIndex = index;
-    }
+    Array.from(document.getElementsByClassName("menu-item")).forEach(
+      (item, index) => {
+        item.onmouseover = () => {
+          menu.dataset.activeIndex = index;
+        };
+      }
+    );
   });
   return (
-    <div id = "menu">
-      <div id = "menu-items">
-        <a href = "/" className = "menu-item">Home</a>
-        <a href = "/" className = "menu-item">Shop</a>
-        <a href = "/" className = "menu-item">About</a>
-        <a href = "/" className = "menu-item">Contact Us</a>
+    <div id="menu">
+      <div id="menu-items">
+        <a href="/" className="menu-item">
+          Home
+        </a>
+        <a href="/" className="menu-item">
+          Shop
+        </a>
+        <a href="/" className="menu-item">
+          About
+        </a>
+        <a href="/" className="menu-item">
+          Contact Us
+        </a>
       </div>
       {/* <div id = "menu-background-pattern"></div> */}
-      <div id = "menu-background-image"></div>
+      <div id="menu-background-image" ref={paraRef}></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
